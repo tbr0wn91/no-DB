3dff5ad12b838926a5a82c0f15e0c25ca8968dfd
thomas brown <thomasbrown9101@gmail.com>
2019-07-04 14:46:30 -0700

    set proxy up and connect front to back end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      lineup: [],
+      avg: "",
+
+    }
+      
+  }
+
+  componentDidMount(){
+    this.GetAllPlayerData();
+  }
+
+  GetAllPlayerData(){
+    axios.get("/api/players").then(response => {
+      this.setState({
+        PlayerData: response.data
+      })
+    }).catch(err => console.log(err));
+    
+  }
+    
+
+  render(){
+    return (
+      <div className="App">
+        Hello World
+        
+      </div>
+    )
+  }
+
+  
 }
 
 export default App;
