380b1b4c3bfde78828aaff6123a373fb7d1672d2
thomas brown <thomasbrown9101@gmail.com>
2019-07-08 09:21:19 -0700

    updated CSS

--- src/components/DisplayName.js
+++ src/components/DisplayName.js
@@ -28,10 +28,12 @@ export default class DisplayName extends Component{
             const mappedPlayers = this.props.PlayerData.map((player, index) => {
                 // console.log(player)
                  return <div key={index} className="player-list">
+                       <div className="display-box"> 
                          <PlayerToDisplay player={player}/>
                          <input onChange={(e) => this.handleUserInput(e.target.value)} type="text"></input>
                          <button onClick={() =>this.props.updatePlayerStats(player.id, this.state.UserInput)}>Update Avg</button>
                          <DeletePlayer deletePlayer={this.props.deletePlayer} id={index}/>
+                       </div>
                  </div>
              });
 
